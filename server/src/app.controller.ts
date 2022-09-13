c0da7297d667b4bc3d570d91c1dcbcfde8543c8a
Thomas Walter <thomasw@hokla.com>
2022-09-13 12:33:29 +0200

    rename secret to token

--- server/src/app.controller.ts
+++ server/src/app.controller.ts
@@ -11,9 +11,9 @@ export class AppController {
   constructor(private readonly appService: AppService) {}
 
   @Get('flag')
-  getFlag(@Headers('secret') secret: string): string {
-    if (!this.appService.isSecretValid(secret)) {
-      throw new UnauthorizedException('Invalid secret');
+  getFlag(@Headers('token') token: string): string {
+    if (!this.appService.isTokenValid(token)) {
+      throw new UnauthorizedException('Invalid token');
     }
 
     return process.env.SECRET_FLAG ?? 'Flag_Not_Defined';
--- server/src/app.service.ts
+++ server/src/app.service.ts
@@ -2,7 +2,7 @@ import { Injectable } from '@nestjs/common';
 
 @Injectable()
 export class AppService {
-  isSecretValid(secret: string): boolean {
-    return secret === '1234';
+  isTokenValid(token: string): boolean {
+    return token === '1234';
   }
 }
