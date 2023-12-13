d321362c73302d58d9f5c2428cb400e0a2ea7728
Maxime Crampon <maximec@hokla.com>
2023-12-13 17:45:19 +0100

    fix(server): remove providing non-existing ceaser service

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -1,10 +1,9 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
-import { CaesarService } from './caesar.service';
 
 @Module({
   imports: [],
   controllers: [AppController],
-  providers: [CaesarService],
+  providers: [],
 })
 export class AppModule {}
